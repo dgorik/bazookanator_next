5d3613f29e89c5e79c87e134bae92c1b0bde74a5
Daniel Gorbachev <dg186533@gmail.com>
2025-04-26 10:06:24 -0400

    added a server side component

---
+++ src/app/api/page.tsx
@@ -0,0 +1,3 @@
+export default function ServerPage() {
+  return <h1>Hello from the server 👋</h1>;
+}
--- src/components/verification/Verification.tsx
+++ src/components/verification/Verification.tsx
@@ -1,5 +1,15 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export function VerificationComponent() {
-  return <div>Boooooom, verify</div>;
+export async function VerificationComponent() {
+  return (
+    <div>
+      <div>Boooooom, verify</div>;
+      <Link href="/api">
+        <Button type="submit" className="w-full">
+          Click to see a server component
+        </Button>
+      </Link>
+    </div>
+  );
 }
