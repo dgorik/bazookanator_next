dbe6a03ed32da997f8b7deedd8b6a52302825b4f
Daniel Gorbachev <dg186533@gmail.com>
2025-05-05 08:15:57 -0400

    added types to authentication route

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,44 +1,42 @@
 import  { NextRequest, NextResponse } from "next/server";
-import validator from 'validator';
+import validator, { normalizeEmail } from 'validator';
 import User from "@/models/User";
 
-// type LoginRequestBody = {
-//   email: string;
-//   password: number;
-// };
-
-type ValidationError = {
-  msg: string;
+type Credentials = {
+  email: string;
+  password: number;
 };
+
+type ValidationError = { msg: string }[]; // ValidationError is an array of objects
 
 export async function POST( ///google this more
   req: NextRequest,
   res: NextResponse //this is a response object in app route
 ) {
   if (req.method === "POST") {
-    const ValidationError = []
+    const ValidationErrors: ValidationError =  []
 
-    let {email, password} = await req.json()
+    const {email, password}: Credentials = await req.json()
 
     if(!email || !password){
-      ValidationError.push({ msg: "Email and password are required" });
+      ValidationErrors.push({ msg: "Email and password are required" });
     }
 
     if (!validator.isEmail(email))
-      ValidationError.push({ msg: "Please enter a valid email address." });
+      ValidationErrors.push({ msg: "Please enter a valid email address." });
     if (!email.endsWith('@bazooka-inc.com')){
-      ValidationError.push({ msg: "Please enter bazooka-inc.com email." });
+      ValidationErrors.push({ msg: "Please enter bazooka-inc.com email." });
     }
     if (validator.isEmpty(email))
-      ValidationError.push({ msg: "Password cannot be blank." });
+      ValidationErrors.push({ msg: "Password cannot be blank." });
   
-    if (ValidationError.length) {
-      return NextResponse.json(ValidationError)
+    if (ValidationErrors.length) {
+      return NextResponse.json(ValidationErrors)
     }
-    email = validator.normalizeEmail(email, {
+    let normalized_email = validator.normalizeEmail(email, {
       gmail_remove_dots: false,
     });
-    return NextResponse.json([{msg: `${email}`}])
+    return NextResponse.json([{msg: `${normalized_email}`}])
 
   } else {
     // Handle other HTTP methods (e.g., GET)
