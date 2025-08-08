cfeff3e4d3d895261c4da97c7d4342cea7558cf9
Daniel Gorbachev <dg186533@gmail.com>
2025-08-08 13:29:00 -0400

    stubbed verify email page

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -6,42 +6,43 @@ import { useRouter } from 'next/navigation'
 import { ButtonLoading } from '../../../components/ui/buttons/button_loading'
 
 export default function VerifyEmailPage() {
-  const router = useRouter()
-  const searchParams = useSearchParams()
+  // const router = useRouter()
+  // const searchParams = useSearchParams()
 
-  const [loading, setLoading] = useState(true)
-  const [message, setMessage] = useState('')
+  // const [loading, setLoading] = useState(true)
+  // const [message, setMessage] = useState('')
 
-  useEffect(() => {
-    const token = searchParams.get('token')
-    const email = searchParams.get('email')
-    if (token && email) {
-      fetch(`/api/auth/verify-email?token=${token}&email=${email}`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.message) {
-            setMessage(data.message)
-            router.push('/')
-          } else {
-            setMessage(data.error || 'Verification failed')
-          }
-        })
-        .catch(() => setMessage('Verification failed, please try again'))
-        .finally(() => setLoading(false))
-    }
-  }, [searchParams])
+  // useEffect(() => {
+  //   const token = searchParams.get('token')
+  //   const email = searchParams.get('email')
+  //   if (token && email) {
+  //     fetch(`/api/auth/verify-email?token=${token}&email=${email}`)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         if (data.message) {
+  //           setMessage(data.message)
+  //           router.push('/')
+  //         } else {
+  //           setMessage(data.error || 'Verification failed')
+  //         }
+  //       })
+  //       .catch(() => setMessage('Verification failed, please try again'))
+  //       .finally(() => setLoading(false))
+  //   }
+  // }, [searchParams])
 
-  return (
-    <div>
-      {loading ? (
-        <div className="flex flex-col items-center">
-          <ButtonLoading />
-        </div>
-      ) : (
-        <p className="text-center mt-4 text-sm text-muted-foreground">
-          {message}
-        </p>
-      )}
-    </div>
-  )
+  // return (
+  //   <div>
+  //     {loading ? (
+  //       <div className="flex flex-col items-center">
+  //         <ButtonLoading />
+  //       </div>
+  //     ) : (
+  //       <p className="text-center mt-4 text-sm text-muted-foreground">
+  //         {message}
+  //       </p>
+  //     )}
+  //   </div>
+  // )
+  return <div> Verify</div>
 }
