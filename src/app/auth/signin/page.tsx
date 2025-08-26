d1e2a69a1ef25ef47078bc2afb9b9283fc516d63
Daniel Gorbachev <dg186533@gmail.com>
2025-08-26 12:08:44 -0400

    disabled config rules

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,9 +1,9 @@
-import LoginForm from "@/app/auth/signin/components/LoginForm";
+import LoginForm from '@/app/auth/signin/components/LoginForm'
 
 export default function SignInPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <LoginForm />
     </div>
-  );
+  )
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,9 +1,9 @@
-import SignupForm from "@/app/auth/signup/components/SignupForm";
+import SignupForm from '@/app/auth/signup/components/SignupForm'
 
 export default function Signup() {
   return (
     <div className=" flex flex-col items-center justify-center min-h-screen">
       <SignupForm />
     </div>
-  );
+  )
 }
