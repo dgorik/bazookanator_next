5d3f8fb3bbd09aac192dd7d556dd3a7d9228d8d7
Daniel Gorbachev <dg186533@gmail.com>
2025-09-03 15:20:10 -0400

    added suspense boundary to update password component

--- src/app/auth/update-password/page.tsx
+++ src/app/auth/update-password/page.tsx
@@ -1,9 +1,12 @@
 import UpdatePassword from '@/src/app/auth/update-password/components/UpdatePassword'
+import { Suspense } from 'react'
 
 export default function UpdatePasswordPage() {
   return (
     <>
-      <UpdatePassword />
+      <Suspense fallback={<div> Loading...</div>}>
+        <UpdatePassword />
+      </Suspense>
     </>
   )
 }
