bc9e5573850209b219fd3e91dd1b31c03b5acf11
Daniel Gorbachev <dg186533@gmail.com>
2025-09-02 21:18:27 -0400

    added suspense for client side component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginForm from './components/LoginForm'
+import { Suspense } from 'react'
 
 export default function SignInPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <LoginForm />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <LoginForm />
+      </div>
+    </Suspense>
   )
 }
