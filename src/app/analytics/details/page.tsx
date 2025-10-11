d265cc700a679b530b88ee7ce0cd960d07e84db7
Daniel Gorbachev <dg186533@gmail.com>
2025-10-10 20:25:02 -0400

    moved usage data to a different file to comply with Next.js rule for what can be exported fromm page.tsx file

--- src/app/analytics/details/page.tsx
+++ src/app/analytics/details/page.tsx
@@ -1,53 +1,6 @@
 import { DataTable } from '@/src/components/ui/data-table/DataTable'
 import { columns } from '@/src/components/ui/data-table/columns'
-
-export const usage = [
-  {
-    division: 'B&M',
-    brand: 'Acme Motors',
-    category: 'Sedans',
-    le: 75000000,
-    boardop: 70000000,
-    plan2025: 82000000,
-    sales2024: 78000000,
-  },
-  {
-    division: 'B&M',
-    brand: 'Globex Corp',
-    category: 'Trucks',
-    le: 120000000,
-    boardop: 125000000,
-    plan2025: 115000000,
-    sales2024: 105000000,
-  },
-  {
-    division: 'Ecomm',
-    brand: 'Soylent Foods',
-    category: 'Snacks',
-    le: 35000000,
-    boardop: 36000000,
-    plan2025: 42000000,
-    sales2024: 38000000,
-  },
-  {
-    division: 'Ecomm',
-    brand: 'Initech Solutions',
-    category: 'Software',
-    le: 95000000,
-    boardop: 98000000,
-    plan2025: 105000000,
-    sales2024: 92000000,
-  },
-  {
-    division: 'B&M',
-    brand: 'Genco Piles',
-    category: 'Heavy Eq.',
-    le: 22000000,
-    boardop: 23500000,
-    plan2025: 25000000,
-    sales2024: 21000000,
-  },
-]
+import { usage } from '@/src/data/usage'
 
 export default function Details() {
   return (
