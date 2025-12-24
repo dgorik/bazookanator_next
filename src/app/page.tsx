b3ee8c8490cc7f890478efc74d07c0d8504913d8
Daniel Gorbachev <dg186533@gmail.com>
2025-12-24 10:51:55 -0500

    Revert "added more testing"
    This reverts commit 226beee1b450febb735e317cedd995ea9686db73.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import dynamic from 'next/dynamic'
 import FAQSection from '@/src/components/layout/sections/Faq'
 import HeroSection from '@/src/components/layout/sections/HeroSection'
